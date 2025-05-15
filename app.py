dbcb15b1ba4f53b9273b35b3d17f5834d7b9525f
Krishnavats56 <krishnavats56@gmail.com>
2025-05-15 12:06:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Docker!"
+    return "Hello from Docker with Gunicorn!"
+
+@app.route('/api')
+def api():
+    return jsonify(message="This is a JSON API response.")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("FLASK_PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
